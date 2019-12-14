49fb95216f8c2a14031cfbac6c26955901890c7a
melia miller <meliajm@gmail.com>
2019-12-14 00:32:02 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -28,9 +28,10 @@ class Song
   def self.genre_count
     genre_hash = {}
     @@genres.each { |gen|
-      genre_hash[gen] = [] if @@genres.keys.include?(gen)
+      genre_hash[gen] = [] if !genre_hash.keys.include?(gen)
       genre_hash[gen] += 1 
     }
+    genre_hash
   end
   
   def self.artist_count
